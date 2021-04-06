c4cf0390e494db25727c6b9ccb04ebb59b77d4ad
haris-hukara <haris.hukara@stu.ibu.edu.ba>
2021-04-06 16:03:57 +0200

    add account dao

--- api/dao/AccountDao.class.php
+++ api/dao/AccountDao.class.php
@@ -2,7 +2,19 @@
 class AccountDao extends BaseDao{
 
   public function add_account($account){
+    return $$this->insert("accounts", $account);
 
+  }
+  public function update_account($account){
+    $$this->update("accounts", $account);
+
+  }
+  public function get_account_by_id($id){
+    return $this->query_unique("SELECT * FROM accounts WHERE id =:id", ["id"=> $id]);
+
+  }
+  public function get_all_accounts(){
+    return $$this->query("SELECT * FROM accounts", []);
 
   }
 
